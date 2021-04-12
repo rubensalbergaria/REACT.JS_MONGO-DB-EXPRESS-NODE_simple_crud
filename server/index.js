1e641e06598f3a5fa65f7525711a1fc381a0b2f2
Rubens Albergaria <rubensalbergaria@gmail.com>
2021-04-12 12:26:17 -0300

    updating

--- server/index.js
+++ server/index.js
@@ -1,14 +1,29 @@
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
+const app = express();
+
+
+const FoodModel = require("./models/Food");
 
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://rubensalbergaria:@crud.gonva.mongodb.net/food?retryWrites=true&w=majority', {
+//conectando com o db:
+mongoose.connect('mongodb+srv://rubensalbergaria:@crud.gonva.mongodb.net/test', { //COLOCAR PASSWORD APÓS rubensalbergaria 
   useNewUrlParser: true,
-})
+  useUnifiedTopology: true, //recomendado pelo terminal por se tratar de versao que ficará deprecada
+}) 
 
-//COLOCAR PASSWORD APÓS rubensalbergaria no link acima
+
+//passando dados pro db:
+app.get('/', async (req, res) => {
+  const food = new FoodModel({ foodName: "Apple", daysSinceIAte: 3 });
+  try {
+    await food.save();
+    res.send("inserted data")
+  } catch (err) {
+    console.log(err)
+  }
+})
 
 app.listen(3001, () => {
   console.log('server is running on port 3001')
