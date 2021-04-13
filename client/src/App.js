b0d855b2cf237ff6b5423d77470e4a4264fabf4d
Rubens Albergaria <rubensalbergaria@gmail.com>
2021-04-13 14:27:30 -0300

    reading data

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,58 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
 
 function App() {
+
+  const [foodName, setFoodName] = useState('');
+  const [days, setDays] = useState(0);
+
+  const [foodList, setFoodList] = useState([]);
+    
+  useEffect(() => {
+    Axios.get("http://localhost:3001/read").then((response) => {
+      setFoodList(response.data);
+    });
+  }, []);
+
+  const addToList = () => { //just send the information. That's why don't need a then
+    console.log(foodName)
+    Axios.post('http://localhost:3001/insert',
+      {
+        foodName: foodName,
+        days: days
+      });
+  }
+
   return (
     <div className="App">
-      <h1>Yooo</h1>
+      <h1>CRUD App with MERN</h1>
+
+      <label>Food name: </label>
+      <input
+        type="text"
+        onChange={(e) => {
+          setFoodName(e.target.value);
+        }}
+      />
+      <label>Days since you ate it: </label>
+      <input
+        type="number"
+        onChange={(e) => {
+          setDays(e.target.value);
+        }}
+      />
+      <button onClick={addToList}>Add to list</button>
+      <h1>Food List: </h1>
+
+      {foodList.map((val, key) => {
+        return (
+          <div key={key}>
+            <h1> Food name: {val.foodName} </h1>
+            <h1> Days since I Ate: {val.daysSinceIAte}</h1>
+          </div>
+        )
+      })}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,28 +1,43 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
-
 
 const FoodModel = require("./models/Food");
 
 app.use(express.json());
+app.use(cors());
 
-//conectando com o db:
-mongoose.connect('mongodb+srv://rubensalbergaria:@crud.gonva.mongodb.net/test', { //COLOCAR PASSWORD APÓS rubensalbergaria 
+mongoose.connect('mongodb+srv://rubensalbergaria:Galodoido84@crud.gonva.mongodb.net/food?retryWrites=true&w=majority', {
+  //COLOCAR PASSWORD APÓS rubensalbergaria e o nome da database após net/
   useNewUrlParser: true,
   useUnifiedTopology: true, //recomendado pelo terminal por se tratar de versao que ficará deprecada
 }) 
 
 
 //passando dados pro db:
-app.get('/', async (req, res) => {
-  const food = new FoodModel({ foodName: "Apple", daysSinceIAte: 3 });
+app.post("/insert", async (req, res) => {
+  const foodName = req.body.foodName;
+  const days = req.body.days;
+
+  const food = new FoodModel({ foodName: foodName, daysSinceIAte: days }); 
   try {
     await food.save();
     res.send("inserted data")
   } catch (err) {
     console.log(err)
   }
+})
+
+
+app.get("/read", async (req, res) => {
+  FoodModel.find({}, (err, result) => {
+    if (err) {
+      res.send(err)
+    }
+
+    res.send(result)
+  })
 })
 
 app.listen(3001, () => {
